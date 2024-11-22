2a8e49bc084458156d427c8eaf7323e94a55c27e
Bruno Hirata <bruno.hirata@vodafone.com>
2024-11-22 11:02:52 +0000

    hat project

---
+++ mini-project-hat-game/js/images.js
@@ -0,0 +1,42 @@
+const IMAGES = [
+  {
+    image: 'images/ball.png',
+    answer: 'b',
+  },
+  // {
+  //   image: 'images/boat.png',
+  //   answer: 'b',
+  // },
+  {
+    image: 'images/hat.png',
+    answer: 'h',
+  },
+  // {
+  //   image: 'images/kite.png',
+  //   answer: 'k',
+  // },
+  // {
+  //   image: 'images/poop.png',
+  //   answer: 'p',
+  // },
+  // {
+  //   image: 'images/popsicle.png',
+  //   answer: 'p',
+  // },
+  // {
+  //   image: 'images/sad.png',
+  //   answer: 's',
+  // },
+  // {
+  //   image: 'images/short.png',
+  //   answer: 's',
+  // },
+  // {
+  //   image: 'images/smily.png',
+  //   answer: 's',
+  // },
+  // {
+  //   image: 'images/star.png',
+  //   answer: 's',
+  // },
+]
